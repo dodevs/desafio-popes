8b39b5553215e97a20e1de83a700c7723d51b746
dodevs <douglass.sousa@outlook.com.br>
2018-09-19 10:11:09 -0300

    Mudança no metodo de conexao, com variavel de ambiente e execucao apos estar conectado ao mongo

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const app = require('express')();
 const port = 5000;
 const mongoose = require('mongoose');
-const Ciente = require('./api/models/clienteModel');
+//const Ciente = require('./api/models/clienteModel');
 const bodyParser = require('body-parser');
 
 // conexao mongoose
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://mongo:27017/desafio", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true});
 
-app.use(bodyParser.json());
+// executar apos conexao
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+    app.use(bodyParser.json());
 
-const routes = require('./api/routes/clienteRouter');
-routes(app); // App é passado para routes e gerencia as rotas lá
+    const routes = require('./api/routes/clienteRouter');
+    routes(app); // App é passado para routes e gerencia as rotas lá
 
-app.listen(port)
+    app.listen(port)
+});
