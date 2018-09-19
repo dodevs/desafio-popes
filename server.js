bbca2cf49414c66ab652fff0182dc9e841fd8552
dodevs <douglass.sousa@outlook.com.br>
2018-09-18 22:47:13 -0300

    Configurações do servidor

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+const port = 5000;
+const mongoose = require('mongoose');
+const schemas = require('./api/models/clienteModel');
+const bodyParser = require('body-parser');
+
+// conexao mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://mongo:27017/desafio', {useNewUrlParser: true});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const routes = require('./api/routes/clienteRouter');
+routes(app); // App é passado para routes e gerencia as rotas lá
+
+app.listen(port)
